d7d8e22ad60a6eaab47e8eba1a7df247cebaabb0
ivan-kyrylko <iva.kyrylko@gmail.com>
2025-12-14 20:43:55 +0200

    next update 0.3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Rajdhani, Orbitron } from "next/font/google";
 
-const raj = Rajdhani({ subsets: ["latin", "cyrillic"], weight: ["400","500","600","700"], variable: "--font-raj" });
+const raj = Rajdhani({ subsets: ["latin"], weight: ["400","500","600","700"], variable: "--font-raj" });
 const orb = Orbitron({ subsets: ["latin"], weight: ["400","500","600","700"], variable: "--font-orb" });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
